fb33470d3abc6ebf4ea5983d5d638f89f7a6af29
gominie <109306648+gominie@users.noreply.com>
2024-08-14 17:06:10 +0100

    -

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+gem 'rubocop', require: false
+source 'https://rubygems.org'
+
+# gem "rails"
---
+++ node.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Node
+  attr_accessor :value, :key, :next_node
+
+  def initialize(key = nil, value = nil)
+    @value = value
+    @key = key
+    @next_node = nil
+  end
+end
